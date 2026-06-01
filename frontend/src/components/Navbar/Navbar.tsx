0fe4dc2b82391f749a230cb446538763b21d19c8
Antoine Huynh <antoine.huynh77@gmail.com>
2026-06-01 07:55:51 +0200

    fix: better responsive

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar: React.FC = React.memo(() => {
-  const { pathname } = useLocation();
-
   return (
     <nav className="navbar">
       <div className="container navbar__inner">
         <Link to="/" className="navbar__brand">
           Box Office <span>Explorer</span>
         </Link>
-        {pathname !== '/' && (
-          <Link to="/" className="back-link">
-            ← Retour à la liste
-          </Link>
-        )}
       </div>
     </nav>
   );
